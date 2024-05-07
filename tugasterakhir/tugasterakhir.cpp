f214cf77569272fbbfda3822cdb615e392b5108f
muhammadrayhanwidayat <murayat99@gmail.com>
2024-05-07 11:33:16 +0700

    membuat class pelajar dan manusia  dengan fungsi public

---
+++ tugasterakhir/tugasterakhir.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class pelajar;
+class manusia {
+public:
+    void showNilaiPelajar(pelajar& a);
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
