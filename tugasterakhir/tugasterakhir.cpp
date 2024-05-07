a9fcf39f5977d87eeba2a8c184ef09ecd21e8d66
muhammadrayhanwidayat <murayat99@gmail.com>
2024-05-07 11:39:18 +0700

    membuat fungsi

--- tugasterakhir/tugasterakhir.cpp
+++ tugasterakhir/tugasterakhir.cpp
@@ -5,8 +5,22 @@ using namespace std;
 class pelajar;
 class manusia {
 public:
-    void showNilaiPelajar(pelajar& a);
+    void showNilaiPelajar(pelajar& x);
 };
+
+class pelajar {
+private:
+    int nilai;
+public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar& x);
+};
+
+void manusia::showNilaiPelajar(pelajar& x) {
+    cout << x.nilai;
+}
+
+
 
 
 int main()
